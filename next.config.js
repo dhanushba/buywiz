e2a73a4b83bf5c3022489663ae609a3fe5c4eb66
dhanushba <dhanushba361@gmail.com>
2026-08-31 23:24:54 +0530

    fix: allow SerpAPI Google CDN images in Next.js image optimization

--- next.config.js
+++ next.config.js
@@ -19,6 +19,22 @@ const nextConfig = {
         hostname: "*.reliancedigital.in",
         protocol: "https",
       },
+      {
+        hostname: "*.gstatic.com",
+        protocol: "https",
+      },
+      {
+        hostname: "*.googleapis.com",
+        protocol: "https",
+      },
+      {
+        hostname: "*.google.com",
+        protocol: "https",
+      },
+      {
+        hostname: "lh3.googleusercontent.com",
+        protocol: "https",
+      },
     ],
   }
 }
